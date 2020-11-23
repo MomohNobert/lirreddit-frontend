4db4859a31b76061d76fe5f7a46eb899cd0d277d
Momoh Nobert <okhai0112@gmail.com>
2020-11-23 09:41:21 +0100

    implementing navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,22 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Link } from '@chakra-ui/react';
 import React from 'react';
+import NextLink from 'next/link';
 
 interface NavBarProps {
 
 }
 
-export const NavBar: React.FC<NavBarProps> = ({}) => {
+export const NavBar: React.FC<NavBarProps> = ({ }) => {
     return (
-        <Box bg='tomato'>
-            pretty good
-        </Box>
+        <Flex bg='tomato' p={4}>
+            <Box ml={'auto'}>
+                <NextLink href="/login">
+                    <Link color='white' mr={4}>login</Link>
+                </NextLink>
+                <NextLink href="/register">
+                    <Link color='white'>register</Link>
+                </NextLink>
+            </Box>
+        </Flex>
     );
 }
