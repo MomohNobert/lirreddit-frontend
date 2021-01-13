618e520db2ea8d8be817ca1035ac0a1f08b1c7fc
Momoh Nobert <okhai0112@gmail.com>
2021-01-13 11:13:37 +0100

    added layout and link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
+import { Link } from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
-import { NavBar } from "../components/NavBar";
+import Layout from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import NextLink  from 'next/link'
 
 const Index = () => {
   const [{ data }] = usePostsQuery();
 
   return (
-  <>
-    <NavBar />
+  <Layout>
+    <NextLink href="/create-post">
+      <Link>
+        create post
+      </Link>
+    </NextLink>
     {!data ? <div>loading...</div> : data.posts.map(post => <div key={post.id}>{post.title}</div>)}
-  </>
+  </Layout>
 )}
 
 export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
