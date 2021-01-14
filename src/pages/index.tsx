ea34381ff68f4b9af744592895fd39283fb8532e
Momoh Nobert <okhai0112@gmail.com>
2021-01-14 15:34:37 +0100

    working on pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,75 @@
-import { Box, Heading, Link, Stack, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
 import Layout from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import NextLink  from 'next/link'
 import React from "react";
+import { useState } from "react";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
-    variables: {
-      limit: 10
-    }
+  const [variables, setVariables] = useState({ 
+    limit: 10, 
+    cursor: null as null | string 
+  })
+  const [{ data, fetching }] = usePostsQuery({
+    variables,
   });
+
+  if (!fetching && !data) {
+    return <div> Your query has failed. </div>
+  }
 
   return (
   <Layout>
-    <NextLink href="/create-post">
-      <Link>
-        create post
-      </Link>
-    </NextLink>
-    {!data ? 
+    <Flex align="center">
+      <Heading>LiReddit</Heading>
+      <NextLink href="/create-post">
+        <Link ml="auto">
+          create post
+        </Link>
+      </NextLink>
+    </Flex>
+    <br />
+    {!data && fetching ? 
       <div>loading...</div> 
       : 
       (
         <Stack spacing={8}>
           {
-            data.posts.map(
+            data!.posts.map(
               post => (
                 <Box p={5} shadow="md" borderWidth="1px" key={post.id}>
                 <Heading fontSize="xl">{post.title}</Heading>
-                <Text mt={4}>{post.text.slice(0, 50)}...</Text>
+                <Text mt={4}>{post.textSnippet} ...</Text>
                 </Box>
               )
             )
           }
         </Stack>
       )
+    }
+    {
+      data ? 
+        (
+          <Flex>
+            <Button 
+              onClick={() => {
+                setVariables({
+                  limit: variables.limit,
+                  cursor: data.posts[data.posts.length - 1].createdAt
+                })
+              }}
+              isLoading={fetching} 
+              m="auto" 
+              my={8}
+            >
+              load more
+            </Button>
+          </Flex> 
+        )
+        : 
+        null
     }
   </Layout>
 )}
